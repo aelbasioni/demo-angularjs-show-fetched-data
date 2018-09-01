51501a0a9f1e098a08a821cda008427843e7ee1d
Asmaa <a.elbasioni@gmail.com>
2018-09-01 05:33:53 +0200

    feat: Add SQLite db and related APIController endpoint as a second datasource option

--- Prices/Controllers/PricesController.cs
+++ Prices/Controllers/PricesController.cs
@@ -6,11 +6,17 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Text;
 using System.IO;
+using System.Data.SQLite;
+using System.Data;
+using Newtonsoft;
 
 namespace Prices.Controllers
 {
     public class PricesController : ApiController
     {
+        /*
+         * Version1: Read the data from JSON file
+         */
         [AllowAnonymous]
         [HttpGet]
         [Route("api/FetchCheesePrices")]
@@ -25,5 +31,71 @@ namespace Prices.Controllers
             };
         }
 
+
+        /*
+         * Version2: Read the data from SQLite database
+         */
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/FetchCheesePrices_v2")]
+        public IHttpActionResult GetPrices_v2()
+        {
+            DataTable dt = new DataTable();
+
+            //Get Sqlite database path:
+            string DBPath = System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/data.db");
+            string connectionString = @"Data Source=" + DBPath + "; Version=3; FailIfMissing=True;";
+
+            //Connect and read the data:
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                string sql = "SELECT * FROM prices";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    using (SQLiteDataAdapter da = new SQLiteDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+
+
+
+            /*
+             * Format the returned JSON data to be as what in data.json: 
+             * */
+            var dtAsEnumerable = dt.AsEnumerable();
+
+            //select distinct the cheese names:
+            List<object> names = dtAsEnumerable
+                   .GroupBy(r => new { name = r["name"] })
+                   .Select(g => g.Key.name).ToList();
+
+
+            //select distinct the years:
+            List<object> years = dt.AsEnumerable()
+                 .GroupBy(r => new { year = r["year"] })
+                 .Select(g => g.Key.year).ToList();
+
+
+            //group the prices for each name:
+            List<object> prices = new List<object>();
+            List<object> prices_details;
+            DataRow[] dr;
+            for (int i = 0; i < names.Count; i++) {
+                prices_details = new List<object>();
+                for (int j = 0; j < years.Count; j++)
+                {
+                    dr = dt.Select("name='" + names[i] + "' and year = " + years[j]);
+                    
+                    //if there's no price in certain year then add 0:
+                    prices_details.Add((dr.Length > 0) ? dr[0]["price"] : 0);
+                }
+                prices.Add(new { name = names[i], price_data = prices_details });
+            }
+            
+            return Ok(new { cheese_data = new {names=names,years=years,prices=prices }});
+        }
+        
     }
 }
