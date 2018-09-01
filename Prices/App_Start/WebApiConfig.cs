11b4bf659d33d0b93aff931e63272d340de1a415
Asmaa <a.elbasioni@gmail.com>
2018-09-01 02:28:35 +0200

    chore: Configure web API to return JSON

--- Prices/App_Start/WebApiConfig.cs
+++ Prices/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace Prices
 {
@@ -19,6 +20,7 @@ namespace Prices
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
