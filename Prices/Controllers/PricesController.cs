0cbbc0526bd6dc0d432381fb70cc9f5a79d33918
Asmaa <a.elbasioni@gmail.com>
2018-09-01 02:36:48 +0200

    feat: Add data (in JSON file) and APIController to access it

---
+++ Prices/Controllers/PricesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Text;
+using System.IO;
+
+namespace Prices.Controllers
+{
+    public class PricesController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/FetchCheesePrices")]
+        public HttpResponseMessage GetPrices()
+        {
+            var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/data.json"));
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+
+    }
+}
